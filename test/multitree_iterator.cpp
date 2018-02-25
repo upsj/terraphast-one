c3d0be11e197b0f1bee213effe82bb7c2617300c
Tobias Ribizel <tobias.ribizel@student.kit.edu>
2018-02-25 17:20:16 +0100

    remove unnecessary test headers

--- test/multitree_iterator.cpp
+++ test/multitree_iterator.cpp
@@ -8,8 +8,6 @@
 #include <terraces/rooting.hpp>
 #include <terraces/subtree_extraction.hpp>
 
-#include "validation_utils.hpp"
-
 #include "../lib/multitree_iterator.hpp"
 #include "../lib/supertree_enumerator.hpp"
 #include "../lib/supertree_variants_multitree.hpp"
@@ -17,6 +15,20 @@
 
 namespace terraces {
 namespace tests {
+
+void check_unique_trees(multitree_node* root, index num_trees) {
+	multitree_iterator it(root);
+
+	std::vector<std::vector<simple_bitvector>> bipartitions;
+	do {
+		bipartitions.push_back(tree_bipartitions(it.tree()));
+	} while (it.next());
+	// check that all trees are unique
+	std::sort(bipartitions.begin(), bipartitions.end());
+	CHECK(std::adjacent_find(bipartitions.begin(), bipartitions.end()) == bipartitions.end());
+	// check that there is the right number of trees
+	CHECK(bipartitions.size() == num_trees);
+}
 
 TEST_CASE("multitree_iterator init simple", "[multitree]") {
 	auto data_stream = std::istringstream{"7 4\n1 1 1 1 s1\n0 0 1 0 s2\n1 1 0 0 s3\n1 1 1 0 "
