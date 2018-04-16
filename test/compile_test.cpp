6d32e71c54654529c2b076a3b096b7e7462eeffa
Tobias Ribizel <tobias.ribizel@student.kit.edu>
2018-04-16 22:24:37 +0200

    fix clang-tidy warning

--- test/compile_test.cpp
+++ test/compile_test.cpp
@@ -7,36 +7,55 @@
 #include <terraces/bigint.hpp>
 #include <terraces/clamped_uint.hpp>
 
-using namespace terraces;
+namespace terraces {
 using namespace terraces::variants;
 using namespace terraces::debug::variants;
 
 template class tree_enumerator<check_callback>;
+
 #ifdef USE_GMP
+
 template class tree_enumerator<count_callback<mpz_class>>;
+
 template class tree_enumerator<count_callback<big_integer>>;
+
 #else
 template class tree_enumerator<count_callback<clamped_uint>>;
 #endif
+
 template class tree_enumerator<clamped_count_callback>;
+
 template class tree_enumerator<multitree_callback>;
 
 template class tree_enumerator<logging_decorator<check_callback>>;
+
 #ifdef USE_GMP
+
 template class tree_enumerator<logging_decorator<count_callback<mpz_class>>>;
+
 template class tree_enumerator<logging_decorator<count_callback<big_integer>>>;
+
 #else
 template class tree_enumerator<logging_decorator<count_callback<clamped_uint>>>;
 #endif
+
 template class tree_enumerator<logging_decorator<clamped_count_callback>>;
+
 template class tree_enumerator<logging_decorator<multitree_callback>>;
 
 template class tree_enumerator<stack_state_decorator<check_callback>>;
+
 #ifdef USE_GMP
+
 template class tree_enumerator<stack_state_decorator<count_callback<mpz_class>>>;
+
 template class tree_enumerator<stack_state_decorator<count_callback<big_integer>>>;
+
 #else
 template class tree_enumerator<stack_state_decorator<count_callback<clamped_uint>>>;
 #endif
+
 template class tree_enumerator<stack_state_decorator<clamped_count_callback>>;
+
 template class tree_enumerator<stack_state_decorator<multitree_callback>>;
+} // namespace terraces
