8f0df578a1fd2da1a20d24962d8cc96114969512
Tobias Ribizel <tobias.ribizel@student.kit.edu>
2018-09-24 01:07:48 +0200

    prepare for 32bit compatibility

--- lib/gcc_clang/intrinsics.hpp
+++ lib/gcc_clang/intrinsics.hpp
@@ -7,7 +7,9 @@ inline index popcount(index word) { return (index)__builtin_popcountll(word); }
 
 inline index bitscan(index word) { return (index)__builtin_ctzll(word); }
 
-inline index rbitscan(index word) { return (index)(63 - __builtin_clzll(word)); }
+inline index rbitscan(index word) {
+	return (index)((std::numeric_limits<index>::digits - 1) - __builtin_clzll(word));
+}
 
 inline bool add_overflow(index a, index b, index& result) {
 	return __builtin_add_overflow(a, b, &result);
--- lib/intel/intrinsics.hpp
+++ lib/intel/intrinsics.hpp
@@ -3,6 +3,7 @@
 namespace terraces {
 namespace bits {
 
+#ifdef _WIN64
 inline index popcount(index word) { return (index)_popcnt64(word); }
 
 inline index bitscan(index word) {
@@ -16,6 +17,21 @@ inline index rbitscan(index word) {
 	_BitScanReverse64(&idx, word);
 	return (index)idx;
 }
+#else
+inline index popcount(index word) { return (index)_popcnt(word); }
+
+inline index bitscan(index word) {
+	unsigned int idx;
+	_BitScanForward(&idx, word);
+	return (index)idx;
+}
+
+inline index rbitscan(index word) {
+	unsigned int idx;
+	_BitScanReverse(&idx, word);
+	return (index)idx;
+}
+#endif
 
 namespace {
 constexpr index max_index = std::numeric_limits<index>::max();
